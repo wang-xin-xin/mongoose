934788f58f1a7ac2655c46e44b64ded24604a12d
wang-xin-xin <wang_xin_xin@outlook.com>
2019-01-18 19:29:07 +0700

    update simplest_web_server to show more error detail

--- examples/simplest_web_server/simplest_web_server.c
+++ examples/simplest_web_server/simplest_web_server.c
@@ -3,7 +3,7 @@
 
 #include "mongoose.h"
 
-static const char *s_http_port = "8000";
+static const char *s_http_port = "8080";
 static struct mg_serve_http_opts s_http_server_opts;
 
 static void ev_handler(struct mg_connection *nc, int ev, void *p) {
@@ -17,12 +17,23 @@ int main(void) {
   struct mg_connection *nc;
 
   mg_mgr_init(&mgr, NULL);
-  printf("Starting web server on port %s\n", s_http_port);
-  nc = mg_bind(&mgr, s_http_port, ev_handler);
-  if (nc == NULL) {
-    printf("Failed to create listener\n");
-    return 1;
-  }
+  // printf("Starting web server on port %s\n", s_http_port);
+  // nc = mg_bind(&mgr, s_http_port, ev_handler);
+  // if (nc == NULL) {
+  //   printf("Failed to create listener\n");
+  //   return 1;
+  // }
+
+struct mg_bind_opts opts;
+memset(&opts, 0, sizeof(opts));
+const char *err = NULL;
+opts.error_string = &err;
+printf("Starting web server on port %s\n", s_http_port);
+nc = mg_bind_opt(&mgr, s_http_port, ev_handler, opts);
+if (nc == NULL) {
+  fprintf(stderr, "mg_bind(%s) failed: %s\n", s_http_port, err);
+  return 1;
+}
 
   // Set up HTTP server parameters
   mg_set_protocol_http_websocket(nc);
